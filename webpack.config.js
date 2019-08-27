3951edfbd6cf948aebd6e8ad339d0f0b5c1a32c3
skw <281431280@qq.com>
2019-08-28 00:44:37 +0800

    a

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 var path = require('path');
+var webpack = require('webpack');
+var packageJSON = require('./package.json');
+
+var ENV = process.env.npm_lifecycle_event;
+
 config = {
         entry: {
             'skw': './src/index.js'
@@ -13,9 +18,6 @@ config = {
             loaders: [
                 {
                     loader: 'babel-loader',
-                    include: [
-                        path.resolve(__dirname, "src")
-                    ],
                     test: path.join(__dirname, 'src'),
                     query: {
                         presets: 'env'
@@ -23,9 +25,31 @@ config = {
                 }
             ]
         },
+        plugins: [
+            new webpack.BannerPlugin(" skw v"+packageJSON.version+"\r\n By https://github.com/fxueye \r\n Github: https://github.com/fxueye/skw\r\n MIT Licensed."),
+            new webpack.NoEmitOnErrorsPlugin()
+        ],
+        stats: {
+            colors: true
+        },
         resolve: {
             // extensions: ['js'],
-          }
+        }
 };
+if(ENV === "build-min"){
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            screw_ie8 : false
+        },
+        mangle: {
+            screw_ie8: false
+        },
+        output: { screw_ie8: false }
+    }));
+    config.entry = {
+        'skw.min': './src/index.js'
+    };
+}
 
 module.exports = config;

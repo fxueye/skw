da6a31f95277a736662ab7c3a323e1d2a2e96fe6
skw <281431280@qq.com>
2019-09-08 22:16:33 +0800

    eventmgr rebuild

--- src/core/EventMgr.js
+++ src/core/EventMgr.js
@@ -1,18 +1,12 @@
 class EventMgr{
     constructor(){
         this._listeners = null
-        this._captureListeners = null
     }
-    addEventListener(type, listener, thisObject,useCapture){
-        var listeners
-        if (useCapture) {
-          listeners = this._captureListeners = this._captureListeners || {}
-        } else {
-          listeners = this._listeners = this._listeners || {}
-        }
+    addEventListener(type, listener, thisObject){
+        var listeners = this._listeners = this._listeners || {}
         var arr = listeners[type]
         if (arr) { 
-            this.removeEventListener(type, listener,thisObject, useCapture) 
+            this.removeEventListener(type, listener,thisObject) 
         }
         arr = listeners[type] 
         if (!arr) { 
@@ -22,8 +16,8 @@ class EventMgr{
         }
         return listener
     }
-    removeEventListener (type, listener,thisObject, useCapture) {
-        var listeners = useCapture ? this._captureListeners : this._listeners
+    removeEventListener (type, listener,thisObject) {
+        var listeners =  this._listeners
         if (!listeners) { return }
         var arr = listeners[type]
         if (!arr) { return }
@@ -37,38 +31,17 @@ class EventMgr{
         })
     }
     dispatchEvent (evt) {
-       
-        var top = this, list = [top]
-        while (top.parent) { list.push(top = top.parent) }
-        var i, l = list.length
-  
-        // capture & atTarget
-        for (i = l - 1; i >= 0 && !evt.propagationStopped; i--) {
-          list[i]._dispatchEvent(evt, 0)
-        }
-        // bubbling
-        for (i = 0; i < l && !evt.propagationStopped; i++) {
-          list[i]._dispatchEvent(evt, 1)
-        }
-      
-    }
-  
-    _dispatchEvent (evt, type) {
       evt.target = this
-      if (this._captureListeners && type === 0) {
-        let cls = this._captureListeners[evt.type]
-        cls && cls.forEach(fn => {
-          fn[0].call(fn[1], evt)
-        })
-      }
-  
-      if (this._listeners && type === 1) {
+      if (this._listeners) {
         let ls = this._listeners[evt.type]
         ls && ls.forEach(fn => {
-            fn[0].call(fn[1], evt)
+          if(fn[1]){
+            fn[0].call(fn[1],evt)
+          }else{
+            fn[0].call(this,evt)
+          }
         })
       }
-    }
-    
+    }    
 }
 export default EventMgr

f281e3c969c7e8600541c7b7b7737bac7993a3e2
skw <281431280@qq.com>
2019-09-10 11:32:54 +0800

    change stringSplit

--- src/core/Utils.js
+++ src/core/Utils.js
@@ -91,13 +91,13 @@ class Utils{
 		var backingStore = ctx.backingStorePixelRatio ||ctx.webkitBackingStorePixelRatio ||ctx.mozBackingStorePixelRatio ||ctx.msBackingStorePixelRatio ||ctx.oBackingStorePixelRatio ||ctx.backingStorePixelRatio || 1;
 		return ((window.devicePixelRatio || 1) / backingStore);
 	}
-	static stringSplit (str, len){
+	static stringSplit (str, len,){
 		let arr = [],
 		offset = 0,
 		char_length = 0
 		for (let i = 0; i < str.length; i++) {
 		let son_str = str.charAt(i)
-		encodeURI(son_str).length > 2 ? char_length += 1 : char_length += 0.5
+		encodeURI(son_str).length > 2 ? char_length += 2 : char_length += 1
 		if (char_length >= len || (char_length < len && i === str.length - 1)) {
 			let sub_len = char_length == len ? i + 1 : i
 			arr.push(str.substr(offset, sub_len - offset + ((char_length < len && i === str.length - 1) ? 1 : 0)))
